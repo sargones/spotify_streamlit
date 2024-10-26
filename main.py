a40f24b78e6116d7fd7e0376ba178da85cbbe299
sargones <pipi.sargones@gmail.com>
2024-10-26 17:16:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_token():
     result = post(url, headers = headers, data = data)
     json_result = json.loads(result.content)
     token = json_result["access_token"]
-    return token
+    return str(token)
 
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
